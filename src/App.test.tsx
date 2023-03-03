188ff37f8f4c0f909630da06ca044817bdf4cc23
Goose <crusty512@yahoo.com>
2023-03-03 15:37:37 -0600

    gh pages

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders start! link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Start!/i);
-  expect(linkElement).toBeInTheDocument();
-});
